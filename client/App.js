984426ca8c7348f9f0eeb17bca9d832f5fcf7ea9
Florian Porada <florian.porada@gmail.com>
2017-09-13 21:13:56 +0200

    added linter + fetching data from (local) backend.

--- client/App.js
+++ client/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View, ListView } from 'react-native';
 
-export default class App extends React.Component {
+class ListItem extends React.Component {
   render() {
     return (
+      <Text>{this.props.name}</Text>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    return fetch('http://192.168.178.117:8080/api/github/users')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson),
+        }, function() {
+          // do something with new state
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <ListItem name={rowData.login} />}
+        />
       </View>
     );
   }
@@ -15,9 +56,12 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
