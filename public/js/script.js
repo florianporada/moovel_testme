2f2613463fe865a4e1b2ca1cf1697b065cb666d2
Florian Porada <florian.porada@gmail.com>
2017-09-13 18:10:35 +0200

    basic frontend with get request for GitHub users

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
-console.log('hi');
+const request = new XMLHttpRequest();
+
+request.open('GET', '/api/github/users/');
+request.addEventListener('load', (event) => {
+  if (request.status >= 200 && request.status < 300) {
+    console.log(event);
+    document.getElementById('code').innerHTML = request.responseText;
+  } else {
+    console.warn(request.statusText, request.responseText);
+  }
+});
+request.send();
