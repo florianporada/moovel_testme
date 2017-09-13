fbc8c0d20dc42514a5a755326376b47a1487bde3
Florian Porada <florian.porada@gmail.com>
2017-09-13 15:12:51 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const winston = require('winston');
+
+winston.log('info', 'hello moovel.');
---
+++ test/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const chai = require('chai');
+const mocha = require('mocha');
+const server = require('../server/index');
+
+mocha.describe('server testing', () => {
+  mocha.it('should do something', () => {
+    const result = server.getUsers();
+    chai.expect(result).to.equal('1');
+  });
+});
