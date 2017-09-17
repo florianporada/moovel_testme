0604d3c0d3e7be9dcb77e264b208fa9e71e41114
Florian Porada <florian.porada@gmail.com>
2017-09-17 18:58:39 +0200

    updated readme file

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -1,12 +1,21 @@
 const request = new XMLHttpRequest();
 
-request.open('GET', '/api/github/users/');
-request.addEventListener('load', (event) => {
-  if (request.status >= 200 && request.status < 300) {
-    console.log(event);
-    document.getElementById('code').innerHTML = request.responseText;
-  } else {
-    console.warn(request.statusText, request.responseText);
-  }
-});
-request.send();
+const callApi = function callApi(endpoint) {
+  request.open('GET', endpoint);
+  request.addEventListener('load', (event) => {
+    if (request.status >= 200 && request.status < 300) {
+      console.log(event);
+      document.getElementById('code').innerHTML = request.responseText;
+    } else {
+      console.warn(request.statusText, request.responseText);
+    }
+  });
+  request.send();
+}
+
+window.onload = function () {
+  document.getElementById('b1').onclick = function onclick() { callApi('/api'); };
+  document.getElementById('b2').onclick = function onclick() { callApi('/api/github/rate_limit'); };
+  document.getElementById('b3').onclick = function onclick() { callApi('/api/github/users/java'); };
+  document.getElementById('b4').onclick = function onclick() { callApi('/api/github/users/moovel'); };
+};
